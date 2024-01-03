a916740b206c5cb087ddb3225c7a0a06353d9c2b
V-ope <opeyemivictor04@gmail.com>
2024-01-03 17:11:16 +0000

    ES6 basic

---
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -0,0 +1,11 @@
+
+rt default function taskBlock(trueOrFalse) {
+	  const task = false;
+	  const task2 = true;
+
+	  if (trueOrFalse) {
+		      return [task, task2];
+		    }
+	  return [task, task2];
+}
+
